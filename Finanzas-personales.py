4fb788a5fb17122d64f63198ee52313533b3bb45
Rodrigo Sebastián Sánchez Vargas <rodrigo.sanchez4@mail.udp.cl>
2026-02-04 02:03:45 -0300

    Mejora en el manejo de errores
    Se incluye manejo de errores para validar entradas numéricas del usuario.

--- Finanzas-personales.py
+++ Finanzas-personales.py
@@ -1,31 +1,47 @@
 name = input("Ingrese su nombre: ")
-print(f"Bienvenido/a {name} a su app de mejora de finanzas personales")
+print(f"Bienvenido/a {name} a tu app de mejora de finanzas personales")
 
-casos = int(input("Ingrese la cantidad de gastos: "))
+while True:
+    try:
+        casos = int(input("Ingrese su numero de gastos: "))
+        break
+    except ValueError:
+        print("Ingrese solo números, refieriendose a su cantidad total de gastos")
 monto = 0
 
 for _ in range(casos):
-    valor = float(input("Ingrese es valor del gasto: $ "))
+    while True:
+        try:
+            valor = float(input("Ingrese el valor del gasto: $"))
+            break
+        except ValueError:
+            print("Ingrese solo números, refieriendose al valor de cada gasto")
     monto += valor
 
-print(f"Gastos totales mensuales: ${monto:.2f}")
+print(f"Gastos totales menusales: ${monto:.1f}")
 
-salario = float(input("Ingrese su salario liquído mensual: $ "))
+while True:
+    try:
+        salario = float(input("Ingrese su salario liquido mensual: $"))
+        break
+    except ValueError:
+        print("Ingrese solo números, refiriendose a su salario liquido mensual")
+
 dinero_total = salario - monto
 porcentaje_gastos = (monto / salario) * 100
 
-print(f"Te quedan ${dinero_total:.2f} disponibles")
-print(f"El porcentaje de tu sueldo destinado a gastos es de: {porcentaje_gastos:.1f} %")
+print(f"Luego de pagar los gasos te quedan: ${dinero_total}")
+print(f"El porcentaje de tu salario destinado a gastos es de: %{porcentaje_gastos:.1f}")
 
 if porcentaje_gastos <= 50:
     print("😁 Excelente control de gastos")
 elif porcentaje_gastos > 50 and porcentaje_gastos <= 80:
-    print("🥶 Cuidado, trata de disminuir gastos inecesarios de ser posible")
+    print("🥶 De ser posible, elimina algun gasto innecesario")
 else:
     print("👹 Cuidado, riesgo financiero alto")
 
 ahorro_recomendado = salario * 0.2
-print(f"Ahorro mensual recomendado: 💰$ {ahorro_recomendado:.2f}")
-print(f"Monto luego de 1 año: $ {ahorro_recomendado * 12:.2f}")
+print(f"Su ahorro recomendado mensual es de: ${ahorro_recomendado:.1f}")
+print(f"El monto de su ahorro luego de un año seria de: {ahorro_recomendado * 12:.1f}")
 
-print(f"Muchas gracias {name} por utilizar la app")
+print(f"Muchas gracias {name} por utilizar la app ;)")
